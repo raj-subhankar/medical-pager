130f790af65fc7281f8679d97b85b29422fa152b
raj-subhankar <subhankar.rj@gmail.com>
2021-09-24 00:04:59 +0530

    Create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelContainer, ChannelListContainer } from "./components";
+import "./App.css";
+
+const apiKey = "qgtk9ttyha7j";
+const client = StreamChat.getInstance(apiKey);
+
 const App = () => {
   return (
-    <div>
-      <h1>Medical Pager Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
+
+export default App;
