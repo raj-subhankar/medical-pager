8202beb59993dee4516ac972b72bf064dc5c9d0c
raj-subhankar <subhankar.rj@gmail.com>
2021-09-24 22:02:20 +0530

    Implement login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-import { ChannelContainer, ChannelListContainer } from "./components";
+import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 import "./App.css";
 
-const apiKey = "qgtk9ttyha7j";
+const apiKey = "xeyanpj2gz8w";
 const client = StreamChat.getInstance(apiKey);
 
+const cookies = new Cookies();
+const authToken = cookies.get("token");
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullName: cookies.get("fullName"),
+      image: cookies.get("avatarURL"),
+      phoneNumber: cookies.get("phoneNumber"),
+      hashedPassword: cookies.get("hashedPassword"),
+    },
+    authToken
+  );
+}
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
