8ba0ce96163dd9638bb8961125b9203a44b8a264
raj-subhankar <subhankar.rj@gmail.com>
2021-09-25 11:55:09 +0530

    Create Channel container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import { ChannelContainer, ChannelListContainer, Auth } from "./components";
 import "./App.css";
+import "stream-chat-react/dist/css/index.css";
 
 const apiKey = "xeyanpj2gz8w";
 const client = StreamChat.getInstance(apiKey);
@@ -26,13 +27,28 @@ if (authToken) {
 }
 
 const App = () => {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
